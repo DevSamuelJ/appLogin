5c800ea0d7ff4024aab47ff9c0e10114fad0c6f6
devsamuelj <samuel701silva@gmail.com>
2026-04-11 23:44:07 -0300

    FUTURE: Instalando as dependencias

--- src/main/java/dev/samuelj/usuariosapi/model/Usuario.java
+++ src/main/java/dev/samuelj/usuariosapi/model/Usuario.java
@@ -5,7 +5,10 @@ import jakarta.persistence.*;
 @Entity
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String nome;
     private int idade;
     private String profissao;
