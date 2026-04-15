144cc58d182b89c2a52022317901676072c8d384
devsamuelj <samuel701silva@gmail.com>
2026-04-14 22:49:55 -0300

    feat: tratamento de erros com handler

--- src/main/java/dev/samuelj/usuariosapi/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/samuelj/usuariosapi/exception/GlobalExceptionHandler.java
@@ -4,13 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-@RestControllerAdvice // essa classe observa todos os controllers
-public class GlobalExceptionHandler {
+@RestControllerAdvice // faz com que essa classe observe todos os controllers
+public class GlobalExceptionHandler { // quando acontecer certo tipo de erro, eu transformo ele em um ErroResponse (molde/corpo do erro)
 
     @ExceptionHandler(ResponseStatusException.class) // quando acontecer esse tipo de erro, eu cuido dele
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+//    @ResponseStatus(HttpStatus.NOT_FOUND) // Sem utilidade no momento, pois se usarmos isso, o método vai ficar fixo em sempre retornar um erro 404 (not fount)
     public ErroResponse handleResponseStatusException(ResponseStatusException ex) {
-        return new ErroResponse(
+        return new ErroResponse( // Isso além de retornar as informações ele instancia um objeto automaticamente da classe ErrorResponde
                 ex.getStatusCode().value(),
                 ex.getReason()
         );
