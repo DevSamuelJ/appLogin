48f338df98e19ab1ff1844851b0922ab19e62356
devsamuelj <samuel701silva@gmail.com>
2026-04-14 23:38:59 -0300

    feat: criando lista de erros

--- src/main/java/dev/samuelj/usuariosapi/exception/ErroResponse.java
+++ src/main/java/dev/samuelj/usuariosapi/exception/ErroResponse.java
@@ -1,12 +1,17 @@
 package dev.samuelj.usuariosapi.exception;
 
+import org.aspectj.weaver.Lint;
+import org.w3c.dom.stylesheets.LinkStyle;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ErroResponse {
 
     private int status;
     private String mensagem;
     private LocalDateTime timestamp;
+    private List<String> erros;
 
     public ErroResponse(int status, String mensagem) {
         this.status = status;
