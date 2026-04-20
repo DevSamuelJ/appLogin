ff73c4b18f2001be4c5001478d4c41e59998093b
devsamuelj <samuel701silva@gmail.com>
2026-04-20 17:24:21 -0300

    Criando lista de erros

--- src/main/java/dev/samuelj/usuariosapi/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/samuelj/usuariosapi/exception/GlobalExceptionHandler.java
@@ -1,20 +1,32 @@
 package dev.samuelj.usuariosapi.exception;
 
-import org.springframework.http.HttpStatus;
+
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.awt.font.FontRenderContext;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice // faz com que essa classe observe todos os controllers
 public class GlobalExceptionHandler { // quando acontecer certo tipo de erro, eu transformo ele em um ErroResponse (molde/corpo do erro)
 
     @ExceptionHandler(ResponseStatusException.class) // quando acontecer esse tipo de erro, eu cuido dele
-//    @ResponseStatus(HttpStatus.NOT_FOUND) // Sem utilidade no momento, pois se usarmos isso, o método vai ficar fixo em sempre retornar um erro 404 (not fount)
     public ErroResponse handleResponseStatusException(ResponseStatusException ex) {
 
         return new ErroResponse( // Isso além de retornar as informações ele instancia um objeto automaticamente da classe ErrorResponde
                 ex.getStatusCode().value(),
                 ex.getReason()
         );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class) // essa parte ta dizendo: se esse tipo de erro acontecer, chama esse método; O .class serve pra pegar a classe.
+    public ErroResponse nomeDoMetodo(MethodArgumentNotValidException ex) {
+        List<String> erros = new ArrayList<>();
+        for(FieldError erro: ex.getBindingResult().getFieldErrors()){
+            
+        }
     }
 }
