7ff180c11276d24f503f4e46be83f5634cea16a8
devsamuelj <samuel701silva@gmail.com>
2026-05-01 18:39:12 -0300

    feat: Ambiente mysql funcional

--- src/main/java/dev/samuelj/usuariosapi/service/UsuarioService.java
+++ src/main/java/dev/samuelj/usuariosapi/service/UsuarioService.java
@@ -21,9 +21,6 @@ public class UsuarioService {
         this.usuarioRepository = usuarioRepository;
     }
 
-//    private int contadorId = 1; // São variáveis de estado da classe.
-//    private List<Usuario> usuarios = new ArrayList<>(); // Variável de estado.
-
     public List<Usuario> listarUsuarios() {
         return usuarioRepository.findAll();
     }
