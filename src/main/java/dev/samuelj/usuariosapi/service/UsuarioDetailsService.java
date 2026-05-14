f78c88976925b7e91c204b273de3415271cd578e
devsamuelj <samuel701silva@gmail.com>
2026-05-14 14:05:45 -0300

    feat: iniciando classe service do Security

---
+++ src/main/java/dev/samuelj/usuariosapi/service/UsuarioDetailsService.java
@@ -0,0 +1,24 @@
+package dev.samuelj.usuariosapi.service;
+
+import dev.samuelj.usuariosapi.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioDetailsService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        usuarioRepository.findByEmail(username);
+        return null;
+    }
+}
