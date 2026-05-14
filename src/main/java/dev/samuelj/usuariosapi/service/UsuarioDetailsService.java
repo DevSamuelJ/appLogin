28c4ad96f9a59619f7b58438d8ed5f1a7e30d908
devsamuelj <samuel701silva@gmail.com>
2026-05-14 16:26:18 -0300

    feat: Método de busca do spring security funcionando!

--- src/main/java/dev/samuelj/usuariosapi/service/UsuarioDetailsService.java
+++ src/main/java/dev/samuelj/usuariosapi/service/UsuarioDetailsService.java
@@ -1,10 +1,13 @@
 package dev.samuelj.usuariosapi.service;
 
+import dev.samuelj.usuariosapi.model.Usuario;
 import dev.samuelj.usuariosapi.repository.UsuarioRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UsuarioDetailsService implements UserDetailsService {
@@ -18,7 +21,10 @@ public class UsuarioDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        usuarioRepository.findByEmail(username);
-        return null;
+        Optional<Usuario> user = usuarioRepository.findByEmail(username);
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("Usuario no encontrado");
+        }
+        return user.get();
     }
 }
