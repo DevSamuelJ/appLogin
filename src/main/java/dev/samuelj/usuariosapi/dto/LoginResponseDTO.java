d91b5b8e3fb302a18179bd4e8cff723256a97164
devsamuelj <samuel701silva@gmail.com>
2026-06-06 02:45:57 -0300

    feat: dto de response para login e inicio da implementação do JWT

---
+++ src/main/java/dev/samuelj/usuariosapi/dto/LoginResponseDTO.java
@@ -0,0 +1,49 @@
+package dev.samuelj.usuariosapi.dto;
+
+
+public class LoginResponseDTO {
+
+    private String token;
+    private int id;
+    private String nome;
+    private String email;
+
+    public LoginResponseDTO(String token, int id, String nome, String email){
+        this.token = token;
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/dev/samuelj/usuariosapi/service/JwtService.java
+++ src/main/java/dev/samuelj/usuariosapi/service/JwtService.java
@@ -1,9 +1,21 @@
 package dev.samuelj.usuariosapi.service;
 
+import dev.samuelj.usuariosapi.dto.UsuarioRequestDTO;
+import dev.samuelj.usuariosapi.model.Usuario;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
-    
 
+    public String generateToken(UsuarioRequestDTO usuario) {
+
+    }
+
+    public boolean isTokenValid(String token) {
+
+    }
+
+    public String extractEmail(String token) {
+
+    }
 }
